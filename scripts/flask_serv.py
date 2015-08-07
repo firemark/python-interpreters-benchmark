615029a41bf8b1d7f9abb00977fb4aaca59bf2cd
Firemark <marpiechula@gmail.com>
2015-08-08 00:20:06 +0200

    add flask hello world

---
+++ scripts/flask_serv.py
@@ -0,0 +1,11 @@
+#from http://flask.pocoo.org/docs/0.10/quickstart/ 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
